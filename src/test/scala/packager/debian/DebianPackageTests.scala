bcfb5c47a2babf634f3fb5f06177a52cd5f166a4
Łukasz Wroński <lukaszwronski98@gmail.com>
2021-06-11 13:08:21 +0200

    Run CI on ubuntu, macOs and windows

--- src/test/scala/packager/debian/DebianPackageTests.scala
+++ src/test/scala/packager/debian/DebianPackageTests.scala
@@ -4,39 +4,46 @@ import com.eed3si9n.expecty.Expecty.expect
 import packager.PackageHelper
 import packager.dmg.DebianPackage
 
+import scala.util.Properties
+
 class DebianPackageTests extends munit.FunSuite with PackageHelper {
 
-  test("should create DEBIAN directory ") {
+  if (Properties.isLinux) {
+    test("should create DEBIAN directory ") {
 
-    val dmgPackage = DebianPackage(echoLauncherPath, buildOptions)
+      val dmgPackage = DebianPackage(echoLauncherPath, buildOptions)
 
-    // create app directory
-    dmgPackage.createDebianDir()
+      // create app directory
+      dmgPackage.createDebianDir()
 
-    val debianDirectoryPath = tmpDir / s"$packageName-deb"
-    val expectedAppDirectoryPath = debianDirectoryPath /"DEBIAN"
-    val expectedEchoLauncherPath = debianDirectoryPath / "usr" / "share" / "scala" / packageName
-    expect(os.isDir(expectedAppDirectoryPath))
-    expect(os.isFile(expectedEchoLauncherPath))
-  }
+      val debianDirectoryPath = tmpDir / s"$packageName-deb"
+      val expectedAppDirectoryPath = debianDirectoryPath / "DEBIAN"
+      val expectedEchoLauncherPath =
+        debianDirectoryPath / "usr" / "share" / "scala" / packageName
+      expect(os.isDir(expectedAppDirectoryPath))
+      expect(os.isFile(expectedEchoLauncherPath))
+    }
 
-  test("should generate dep package") {
+    test("should generate dep package") {
 
-    val depPackage = DebianPackage(echoLauncherPath, buildOptions)
+      val depPackage = DebianPackage(echoLauncherPath, buildOptions)
 
-    // create dmg package
-    depPackage.build()
+      // create dmg package
+      depPackage.build()
 
-    val expectedDepPath = tmpDir / s"$packageName.deb"
-    expect(os.exists(expectedDepPath))
+      val expectedDepPath = tmpDir / s"$packageName.deb"
+      expect(os.exists(expectedDepPath))
 
-    // list files which will be installed
-    val payloadFiles = os.proc("dpkg", "--contents", expectedDepPath).call().out.text().trim
-    val expectedScriptPath = os.RelPath("usr") / "bin" / packageName
-    val expectedEchoLauncherPath = os.RelPath("usr") / "share" / "scala" / packageName
+      // list files which will be installed
+      val payloadFiles =
+        os.proc("dpkg", "--contents", expectedDepPath).call().out.text().trim
+      val expectedScriptPath = os.RelPath("usr") / "bin" / packageName
+      val expectedEchoLauncherPath =
+        os.RelPath("usr") / "share" / "scala" / packageName
 
-    expect(payloadFiles contains s"./$expectedScriptPath")
-    expect(payloadFiles contains s"./$expectedEchoLauncherPath")
+      expect(payloadFiles contains s"./$expectedScriptPath")
+      expect(payloadFiles contains s"./$expectedEchoLauncherPath")
+    }
   }
 
 }
--- src/test/scala/packager/dmg/DmgPackageTests.scala
+++ src/test/scala/packager/dmg/DmgPackageTests.scala
@@ -4,30 +4,35 @@ import com.eed3si9n.expecty.Expecty.expect
 import packager.PackageHelper
 import packager.macOs.dmg.DmgPackage
 
+import scala.util.Properties
+
 class DmgPackageTests extends munit.FunSuite with PackageHelper {
 
-  test("should create app directory for dmg") {
+  if (Properties.isMac) {
+    test("should create app directory for dmg") {
 
-    val dmgPackage = DmgPackage(echoLauncherPath, buildOptions)
+      val dmgPackage = DmgPackage(echoLauncherPath, buildOptions)
 
-    // create app directory
-    dmgPackage.createAppDirectory()
+      // create app directory
+      dmgPackage.createAppDirectory()
 
-    val expectedAppDirectoryPath = tmpDir / s"$packageName.app"
-    val expectedEchoLauncherPath = expectedAppDirectoryPath / "Contents" / "MacOS" / packageName
-    expect(os.isDir(expectedAppDirectoryPath))
-    expect(os.isFile(expectedEchoLauncherPath))
-  }
+      val expectedAppDirectoryPath = tmpDir / s"$packageName.app"
+      val expectedEchoLauncherPath =
+        expectedAppDirectoryPath / "Contents" / "MacOS" / packageName
+      expect(os.isDir(expectedAppDirectoryPath))
+      expect(os.isFile(expectedEchoLauncherPath))
+    }
 
-  test("should generate dmg package") {
+    test("should generate dmg package") {
 
-    val dmgPackage = DmgPackage(echoLauncherPath, buildOptions)
+      val dmgPackage = DmgPackage(echoLauncherPath, buildOptions)
 
-    // create dmg package
-    dmgPackage.build()
+      // create dmg package
+      dmgPackage.build()
 
-    val expectedDmgPath = tmpDir / s"$packageName.dmg"
-    expect(os.exists(expectedDmgPath))
+      val expectedDmgPath = tmpDir / s"$packageName.dmg"
+      expect(os.exists(expectedDmgPath))
+    }
   }
 
 }
