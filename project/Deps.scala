82cc446fb4750728be238dbc9f0b1f8e2d00d0f1
Scala Steward <me@scala-steward.org>
2021-11-15 14:57:29 +0100

    Update os-lib to 0.7.8

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 object Deps {
   def expecty = "com.eed3si9n.expecty" %% "expecty" % "0.15.4"
   def munit = "org.scalameta" %% "munit" % "0.7.25"
-  def osLib = "com.lihaoyi" %% "os-lib" % "0.7.5"
+  def osLib = "com.lihaoyi" %% "os-lib" % "0.7.8"
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.1.0-M9"
   def thumbnailator = "net.coobird" % "thumbnailator" % "0.4.14"
   def image4j = "org.jclarion" % "image4j" % "0.7"
