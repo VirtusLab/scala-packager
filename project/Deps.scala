ba036f4cd1477de1f0199046553b97f995793302
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-01 14:44:30 +0200

    Update case-app to 2.1.0-M24 (#112)
    Co-authored-by: Łukasz Wroński <lukaszwronski98@gmail.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ object Deps {
   def expecty = "com.eed3si9n.expecty" %% "expecty" % "0.16.0"
   def munit = "org.scalameta" %% "munit" % "0.7.29"
   def osLib = "com.lihaoyi" %% "os-lib" % "0.9.1"
-  def caseApp = "com.github.alexarchambault" %% "case-app" % "2.1.0-M17"
+  def caseApp = "com.github.alexarchambault" %% "case-app" % "2.1.0-M24"
   def thumbnailator = "net.coobird" % "thumbnailator" % "0.4.17"
   def image4j = "org.jclarion" % "image4j" % "0.7"
   def jib = "com.google.cloud.tools" % "jib-core" % "0.24.0"
