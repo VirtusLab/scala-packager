30255164aead59a438bf6aa62f7f46718de93d45
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-01 13:35:11 +0200

    Update jib-core to 0.24.0 (#119)
    Co-authored-by: Łukasz Wroński <lukaszwronski98@gmail.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,6 +7,6 @@ object Deps {
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.1.0-M17"
   def thumbnailator = "net.coobird" % "thumbnailator" % "0.4.17"
   def image4j = "org.jclarion" % "image4j" % "0.7"
-  def jib = "com.google.cloud.tools" % "jib-core" % "0.22.0"
+  def jib = "com.google.cloud.tools" % "jib-core" % "0.24.0"
   def commonsIo = "commons-io" % "commons-io" % "2.12.0"
 }
