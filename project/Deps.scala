8863d85b27df0e4629b466656064bd3be2c3fc94
Scala Steward <scala_steward@virtuslab.com>
2024-11-14 00:47:34 +0000

    Update expecty to 0.17.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Deps {
-  def expecty = "com.eed3si9n.expecty" %% "expecty" % "0.16.0"
+  def expecty = "com.eed3si9n.expecty" %% "expecty" % "0.17.0"
   def munit = "org.scalameta" %% "munit" % "0.7.29"
   def osLib = "com.lihaoyi" %% "os-lib" % "0.9.1"
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.1.0-M24"
