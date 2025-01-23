3b869f7fc138899e7e973e736b7f42b928b5a5ec
Scala Steward <scala_steward@virtuslab.com>
2025-01-23 02:07:01 +0000

    Update munit to 1.1.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Deps {
   def expecty       = "com.eed3si9n.expecty"       %% "expecty"       % "0.17.0"
-  def munit         = "org.scalameta"              %% "munit"         % "1.0.4"
+  def munit         = "org.scalameta"              %% "munit"         % "1.1.0"
   def osLib         = "com.lihaoyi"                %% "os-lib"        % "0.11.3"
   def caseApp       = "com.github.alexarchambault" %% "case-app"      % "2.1.0-M29"
   def thumbnailator = "net.coobird"                 % "thumbnailator" % "0.4.20"
