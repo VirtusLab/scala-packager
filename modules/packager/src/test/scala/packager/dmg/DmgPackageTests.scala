b6aa3904e61ea931cecd7849df3747785d897e42
Piotr Chabelski <ged.subfan@gmail.com>
2025-02-28 10:43:56 +0100

    Add a custom `munit` tag for flaky MacOS tests

--- modules/packager/src/test/scala/packager/dmg/DmgPackageTests.scala
+++ modules/packager/src/test/scala/packager/dmg/DmgPackageTests.scala
@@ -1,6 +1,7 @@
 package packager.dmg
 
 import com.eed3si9n.expecty.Expecty.expect
+import munit.Tag
 import packager.{NativePackageHelper, TestUtils}
 import packager.config.MacOSSettings
 import packager.mac.dmg.DmgPackage
@@ -8,93 +9,88 @@ import packager.mac.dmg.DmgPackage
 import scala.util.Properties
 
 class DmgPackageTests extends munit.FunSuite with NativePackageHelper {
-  override def munitFlakyOK: Boolean = Properties.isMac && !TestUtils.isAarch64
+  override def munitFlakyOK: Boolean = true
+
+  private val FlakyMacOSx86_64 = new Tag("FlakyMacOSx86_64")
+
+  // flakiness on x86_64 MacOS is caused by hdiutil
+  // FIXME: find a way to make these reliable
+  // more context: https://github.com/VirtusLab/scala-cli/pull/2579
+  override def munitTestTransforms: List[TestTransform] =
+    List(
+      new TestTransform(
+        "Flaky MacOS x86_64",
+        { test =>
+          if (test.tags.contains(FlakyMacOSx86_64) && Properties.isMac && !TestUtils.isAarch64)
+            test.tag(munit.Flaky)
+          else test
+        }
+      )
+    ) ++ super.munitTestTransforms
 
   override def outputPackagePath: os.Path = tmpDir / s"scalafmt.dmg"
 
+  def shouldCreateAppDirectoryForDmg(): Unit = {
+    val dmgPackage = DmgPackage(buildSettings)
+
+    // create app directory
+    dmgPackage.createAppDirectory()
+
+    val expectedAppDirectoryPath = tmpDir / s"$packageName.app"
+    val expectedLauncherPath =
+      expectedAppDirectoryPath / "Contents" / "MacOS" / packageName
+    expect(os.isDir(expectedAppDirectoryPath))
+    expect(os.isFile(expectedLauncherPath))
+  }
+
+  def shouldGenerateDmgPackage(): Unit = {
+    val dmgPackage = DmgPackage(buildSettings)
+
+    // create dmg package
+    dmgPackage.build()
+
+    expect(os.exists(outputPackagePath))
+  }
+
+  def shouldOverrideGeneratedDmgPackage(): Unit = {
+    val dmgPackage = DmgPackage(buildSettings)
+
+    // create twice dmg package
+    dmgPackage.build()
+    dmgPackage.build()
+
+    expect(os.exists(outputPackagePath))
+  }
+
+  def sizeDmgPackageShouldBeSimilarToTheApp(): Unit = {
+    val dmgPackage   = DmgPackage(buildSettings)
+    val launcherSize = os.size(scalafmtLauncherPath)
+
+    // create dmg package
+    dmgPackage.build()
+
+    expect(os.exists(outputPackagePath))
+
+    val dmgPackageSize = os.size(outputPackagePath)
+
+    expect(dmgPackageSize < launcherSize + (1024 * 1024))
+  }
+
   if (Properties.isMac) {
-    def shouldCreateAppDirectoryForDmg(): Unit = {
-      val dmgPackage = DmgPackage(buildSettings)
-
-      // create app directory
-      dmgPackage.createAppDirectory()
-
-      val expectedAppDirectoryPath = tmpDir / s"$packageName.app"
-      val expectedLauncherPath =
-        expectedAppDirectoryPath / "Contents" / "MacOS" / packageName
-      expect(os.isDir(expectedAppDirectoryPath))
-      expect(os.isFile(expectedLauncherPath))
+    test("should create app directory for dmg".tag(FlakyMacOSx86_64)) {
+      shouldCreateAppDirectoryForDmg()
     }
 
-    def shouldGenerateDmgPackage(): Unit = {
-      val dmgPackage = DmgPackage(buildSettings)
-
-      // create dmg package
-      dmgPackage.build()
-
-      expect(os.exists(outputPackagePath))
+    test("should generate dmg package".tag(FlakyMacOSx86_64)) {
+      shouldGenerateDmgPackage()
     }
 
-    def shouldOverrideGeneratedDmgPackage(): Unit = {
-      val dmgPackage = DmgPackage(buildSettings)
-
-      // create twice dmg package
-      dmgPackage.build()
-      dmgPackage.build()
-
-      expect(os.exists(outputPackagePath))
+    test("should override generated dmg package".tag(FlakyMacOSx86_64)) {
+      shouldOverrideGeneratedDmgPackage()
     }
 
-    def sizeDmgPackageShouldBeSimilarToTheApp(): Unit = {
-      val dmgPackage   = DmgPackage(buildSettings)
-      val launcherSize = os.size(scalafmtLauncherPath)
-
-      // create dmg package
-      dmgPackage.build()
-
-      expect(os.exists(outputPackagePath))
-
-      val dmgPackageSize = os.size(outputPackagePath)
-
-      expect(dmgPackageSize < launcherSize + (1024 * 1024))
-    }
-
-    if (TestUtils.isAarch64) {
-      test("should create app directory for dmg") {
-        shouldCreateAppDirectoryForDmg()
-      }
-
-      test("should generate dmg package") {
-        shouldGenerateDmgPackage()
-      }
-
-      test("should override generated dmg package") {
-        shouldOverrideGeneratedDmgPackage()
-      }
-
-      test("size dmg package should be similar to the app") {
-        sizeDmgPackageShouldBeSimilarToTheApp()
-      }
-    }
-    else {
-      // flakiness on x86_64 MacOS is caused by hdiutil
-      // FIXME: find a way to make this reliable
-      // more context: https://github.com/VirtusLab/scala-cli/pull/2579)
-      test("should create app directory for dmg".flaky) {
-        shouldCreateAppDirectoryForDmg()
-      }
-
-      test("should generate dmg package".flaky) {
-        shouldGenerateDmgPackage()
-      }
-
-      test("should override generated dmg package".flaky) {
-        shouldOverrideGeneratedDmgPackage()
-      }
-
-      test("size dmg package should be similar to the app".flaky) {
-        sizeDmgPackageShouldBeSimilarToTheApp()
-      }
+    test("size dmg package should be similar to the app".tag(FlakyMacOSx86_64)) {
+      sizeDmgPackageShouldBeSimilarToTheApp()
     }
   }
 
