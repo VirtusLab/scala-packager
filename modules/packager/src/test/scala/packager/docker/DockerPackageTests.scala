8194f5cc020980eb5180decdf2975cd990b7bde8
Bartłomiej Tomala <bartek@gwi.com>
2025-10-01 08:58:19 +0200

    add extra directories to the docker image

--- modules/packager/src/test/scala/packager/docker/DockerPackageTests.scala
+++ modules/packager/src/test/scala/packager/docker/DockerPackageTests.scala
@@ -5,7 +5,6 @@ import packager.PackagerHelper
 import packager.config.DockerSettings
 
 import java.nio.file.Paths
-
 import scala.util.Properties
 
 class DockerPackageTests extends munit.FunSuite with PackagerHelper {
@@ -19,8 +18,7 @@ class DockerPackageTests extends munit.FunSuite with PackagerHelper {
       // build docker image
       dockerPackage.build()
 
-      val expectedImage =
-        s"$repository:$qualifier"
+      val expectedImage  = s"$repository:$qualifier"
       val expectedOutput = s"scalafmt $scalafmtVersion"
 
       val output = os
@@ -41,12 +39,45 @@ class DockerPackageTests extends munit.FunSuite with PackagerHelper {
       // build docker image
       dockerPackage.build()
 
-      val expectedImage =
-        s"$repository:$qualifier"
+      val expectedImage  = s"$repository:$qualifier"
       val expectedOutput = s"scalafmt $scalafmtVersion"
 
       val output = os
         .proc("docker", "run", expectedImage, "--version")
+        .call(cwd = os.root)
+        .out
+        .text()
+        .trim
+
+      expect(output == expectedOutput)
+
+      // clear
+      os.proc("docker", "rmi", "-f", expectedImage).call(cwd = os.root)
+    }
+    test("should build docker image with additional directories") {
+      val extraDir  = os.temp.dir(prefix = "extra-")
+      val extraFile = extraDir / "main.scala"
+      println(extraDir)
+      os.write(
+        extraFile,
+        s"""@main def main(): Unit = println("Hello World")""".stripMargin
+      )
+      val config =
+        s"""version = $scalafmtVersion
+           |runner.dialect = scala3""".stripMargin
+
+      val appSettings = buildSettings.copy(
+        extraDirectories = List(extraDir.toNIO)
+      )
+      val dockerPackage = DockerPackage(scalafmtLauncherPath, appSettings)
+      // build docker image
+      dockerPackage.build()
+
+      val expectedImage  = s"$repository:$qualifier"
+      val expectedOutput = "All files are formatted with scalafmt :)"
+
+      val output = os
+        .proc("docker", "run", expectedImage, "--config-str", config, "--test", "/")
         .call(cwd = os.root)
         .out
         .text()
@@ -66,7 +97,8 @@ class DockerPackageTests extends munit.FunSuite with PackagerHelper {
       repository = repository,
       tag = Some(qualifier),
       exec = Some("sh"),
-      dockerExecutable = Some(Paths.get("docker"))
+      dockerExecutable = Some(Paths.get("docker")),
+      extraDirectories = Nil
     )
 
 }
