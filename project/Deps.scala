7248b8954151818951b792337678d41ef6ec850a
Scala Steward <scala_steward@virtuslab.com>
2025-11-18 04:53:24 +0000

    Update jib-core to 0.28.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,6 +7,6 @@ object Deps {
   def caseApp       = "com.github.alexarchambault" %% "case-app"      % "2.1.0"
   def thumbnailator = "net.coobird"                 % "thumbnailator" % "0.4.21"
   def image4j       = "org.jclarion"                % "image4j"       % "0.7"
-  def jib           = "com.google.cloud.tools"      % "jib-core"      % "0.27.3"
+  def jib           = "com.google.cloud.tools"      % "jib-core"      % "0.28.0"
   def commonsIo     = "commons-io"                  % "commons-io"    % "2.21.0"
 }
